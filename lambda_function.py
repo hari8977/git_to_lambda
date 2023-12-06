eee1ef3714b68761b9f35b02e7aa4e3285464533
Harikrishna Siramdasu <sharikrishna@pkrmsezdc.com>
2023-12-06 18:11:39 +0530

    aa

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,8 @@
-# import pandas as pd
-
+import pandas as pd
+import requests
 def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
-    print(d)
+    df = pd.DataFrame(data=d)
+    print(df)
     print('Done x1')
     return d
