2b66ae2afdecb9e477b9e22ebc19e717e2dc2b70
Harikrishna Siramdasu <sharikrishna@pkrmsezdc.com>
2023-12-06 19:28:43 +0530

    aa

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,9 @@
 # import pandas as pd
 # import requests
+import os
 def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
     # df = pd.DataFrame(data=d)
-    print(d)
+    print(os.getenv("URI"))
     print('Done x1')
     return d
